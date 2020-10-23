9594a3fabe27cfd07aa6b0e192a10b2367ca2483
Ping YANG <ping.yang@hotmail.fr>
2020-10-23 11:20:53 +0200

    Ajout du cube rotatif pour les projets

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var cube = document.getElementById('cube');
+var menu = document.getElementById('project-menu');
+
+function rotCube(face, obj) {
+    clearMenu();
+    obj.classList += " active";
+    cube.className = face;
+}
+
+function clearMenu() {
+    menu.childNodes.forEach((item, index) => {
+        if (item.nodeName.includes("LI")) {
+            if (item.className.includes(" active")) {
+                item.className = item.className.replace(" active", "");
+            }    
+        }
+    });
+}
