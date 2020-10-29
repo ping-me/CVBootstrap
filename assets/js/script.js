e818015b9e43a7462bb0be7612338aabbafd101a
Ping YANG <ping.yang.dev@gmail.com>
2020-10-29 09:09:57 +0100

    Nettoyage JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
-var cube = document.getElementById('cube');
+// Initialisation de la liste des projets
 var menu = document.getElementById('project-menu');
 var projectTitle = document.getElementById('project-title');
 var projectDescription = document.getElementById('project-description');
 var projectList = JSON.parse('{ "show-front": { "cube-index": 1, "title": "Refonte du site du bar Le Loot", "description": "Le site web du bar Le Loot a été entièrement refait en utilisant le framework Bootstrap" } , "show-right": { "cube-index": 2, "title": "Projet 2 à venir", "description": "Description à venir..." } , "show-back": { "cube-index": 3, "title": "Projet 3 à venir", "description": "Description à venir..." } , "show-left": { "cube-index": 4, "title": "Projet 4 à venir", "description": "Description à venir..." } , "show-bottom": { "cube-index": 5, "title": "Projet 5 à venir", "description": "Description à venir..." } , "show-top": { "cube-index": 6, "title": "CV Numérique", "description": "Mon CV en ligne, réalisé avec le framework Bootstrap." } }');
+projectDescFill('show-front');
 
-// Initialisation de la liste des projets
-projectTitle.innerHTML = projectList['show-front']['title'];
-projectDescription.innerHTML = projectList['show-front']['description'];
-
-//Initialisation Hammer pour le cube drag
+//Initialisation cube
+var cube = document.getElementById('cube');
 var deltaX = 0;
 var deltaY = 0;
 var lastX = 0;
@@ -18,6 +16,7 @@ hammer.get('pan').set({direction: Hammer.DIRECTION_ALL});
 hammer.on('pan', ev => dragCube(getOffset(ev.deltaX, deltaX), getOffset(-ev.deltaY, deltaY)));
 hammer.on('panend', ev => { deltaX = lastY; deltaY = lastX; });
 
+// Fonctions
 function dragCube(distY, distX) {
     cube.style.transition = '';
     cube.style.transform = `translateZ(-150px) rotateY(${distY}deg) rotateX(${distX}deg)`;
@@ -29,8 +28,7 @@ function getOffset(dist, delta) {
     return dist / 2 + delta;
 }
 
-function rotCube(face, obj)
-{
+function rotCube(face, obj) {
     cube.style.transition = 'transform 1s';
     cube.style.transform = '';
     clearMenu();
@@ -41,14 +39,10 @@ function rotCube(face, obj)
     setTimeout(function() {projectDescShow(true);}, 500);
 }
 
-function clearMenu()
-{
-    menu.childNodes.forEach((item, index) =>
-    {
-        if (item.nodeName.includes("LI"))
-        {
-            if (item.className.includes("active"))
-            {
+function clearMenu() {
+    menu.childNodes.forEach((item, index) => {
+        if (item.nodeName.includes("LI")) {
+            if (item.className.includes("active")) {
                 item.classList.remove("active");
             }
         }
@@ -61,20 +55,20 @@ function projectDescFill(face) {
 }
 
 function projectDescShow(showStatus) {
+    var toRemove = '';
+    var toAdd = '';
     if (showStatus) {
-        if (projectTitle.className.includes("project-fade-out")) {
-            projectTitle.classList.remove("project-fade-out");
-            projectDescription.classList.remove("project-fade-out");
-        }
-        projectTitle.classList.add("project-fade-in");
-        projectDescription.classList.add("project-fade-in");
+        toRemove = 'out';
+        toAdd = 'in';
     }
     else {
-        if (projectTitle.className.includes("project-fade-in")) {
-            projectTitle.classList.remove("project-fade-in");
-            projectDescription.classList.remove("project-fade-in");
-        }
-        projectTitle.classList.add("project-fade-out");
-        projectDescription.classList.add("project-fade-out");
+        toRemove = 'in';
+        toAdd = 'out';
     }
+    if (projectTitle.className.includes("project-fade-" + toRemove)) {
+        projectTitle.classList.remove("project-fade-" + toRemove);
+        projectDescription.classList.remove("project-fade-" + toRemove);
+    }
+    projectTitle.classList.add("project-fade-" + toAdd);
+    projectDescription.classList.add("project-fade-" + toAdd);
 }
