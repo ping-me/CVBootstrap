805b950a08fb9ace4df1947a00f4a37367c16275
Ping YANG <ping.yang.dev@gmail.com>
2020-11-19 17:00:09 +0100

    Ajout age dynamique et correction mineure sur rotation cube

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
 // Initialisation de la liste des projets
-var menu = document.getElementById('project-menu');
-var projectTitle = document.getElementById('project-title');
-var projectDescription = document.getElementById('project-description');
-var projectList = JSON.parse('{ "show-front": { "cube-index": 1, "title": "Refonte du site du bar Le Loot", "description": "Le site web du bar Le Loot a été entièrement refait en utilisant le framework Bootstrap" } , "show-right": { "cube-index": 2, "title": "Projet 2 à venir", "description": "Description à venir..." } , "show-back": { "cube-index": 3, "title": "Projet 3 à venir", "description": "Description à venir..." } , "show-left": { "cube-index": 4, "title": "Projet 4 à venir", "description": "Description à venir..." } , "show-bottom": { "cube-index": 5, "title": "Projet 5 à venir", "description": "Description à venir..." } , "show-top": { "cube-index": 6, "title": "CV Numérique", "description": "Mon CV en ligne, réalisé avec le framework Bootstrap." } }');
+let menu = document.getElementById('project-menu');
+let projectTitle = document.getElementById('project-title');
+let projectDescription = document.getElementById('project-description');
+let projectList = JSON.parse('{ "show-front": { "cube-index": 1, "title": "Refonte du site du bar Le Loot", "description": "Le site web du bar Le Loot a été entièrement refait en utilisant le framework Bootstrap" } , "show-right": { "cube-index": 2, "title": "Projet 2 à venir", "description": "Description à venir..." } , "show-back": { "cube-index": 3, "title": "Projet 3 à venir", "description": "Description à venir..." } , "show-left": { "cube-index": 4, "title": "Projet 4 à venir", "description": "Description à venir..." } , "show-bottom": { "cube-index": 5, "title": "Projet 5 à venir", "description": "Description à venir..." } , "show-top": { "cube-index": 6, "title": "CV Numérique", "description": "Mon CV en ligne, réalisé avec le framework Bootstrap." } }');
 projectDescFill('show-front');
 
-//Initialisation cube
-var cube = document.getElementById('cube');
-var deltaX = 0;
-var deltaY = 0;
-var lastX = 0;
-var lastY = 0;
+// Initialisation cube
+let cube = document.getElementById('cube');
+let deltaX = 0;
+let deltaY = 0;
+let lastX = 0;
+let lastY = 0;
 const hammer = new Hammer(document.getElementById('project-diap'));
 hammer.get('pan').set({direction: Hammer.DIRECTION_ALL});
 hammer.on('pan', ev => dragCube(getOffset(ev.deltaX, deltaX), getOffset(-ev.deltaY, deltaY)));
 hammer.on('panend', ev => { deltaX = lastY; deltaY = lastX; });
 
+// Calcul et affichage de l'age
+document.getElementById('dynamic-age').textContent = getAge();
+
 // Fonctions
+function getAge() {
+    let birthday = new Date('1979-12-26 15:30');
+    let today = new Date();
+    let age = new Date(today - birthday);
+    return age.getFullYear() - 1970;
+}
+
 function dragCube(distY, distX) {
     cube.style.transition = '';
     cube.style.transform = `translateZ(-150px) rotateY(${distY}deg) rotateX(${distX}deg)`;
-    lastX = distX;
-    lastY = distY;
+    lastX = distX % 360;
+    lastY = distY % 360;
 }
 
 function getOffset(dist, delta) {
@@ -55,8 +65,8 @@ function projectDescFill(face) {
 }
 
 function projectDescShow(showStatus) {
-    var toRemove = '';
-    var toAdd = '';
+    let toRemove = '';
+    let toAdd = '';
     if (showStatus) {
         toRemove = 'out';
         toAdd = 'in';
